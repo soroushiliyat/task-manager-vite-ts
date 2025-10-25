624cbbde1dd4c3b76f994ca3d6ae20d4b567fa8f
soroushiliyat <soroushiliyat23@gmail.com>
2025-10-25 15:55:39 +0330

    Update TaskListPage.tsx

--- src/features/tasks/pages/TaskListPage.tsx
+++ src/features/tasks/pages/TaskListPage.tsx
@@ -75,7 +75,7 @@ export default function TaskListPage() {
 
   return (
     <div className="max-w-2xl mx-auto py-8 px-4">
-      <h1 className="text-3xl font-bold mb-6 text-center">لیست تسک‌ها</h1>
+      <h1 className="text-3xl font-bold mb-6 text-center">Tasks list</h1>
 
       <TaskForm onSubmit={handleAddOrEdit} editingTask={editingTask} />
 
