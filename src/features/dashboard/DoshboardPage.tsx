151eeb02eed594169cc2c6ef709c29a928853421
soroushiliyat <soroushiliyat23@gmail.com>
2025-10-25 15:28:20 +0330

    Update DoshboardPage.tsx

--- src/features/dashboard/DoshboardPage.tsx
+++ src/features/dashboard/DoshboardPage.tsx
@@ -8,10 +8,10 @@ export default function DashboardPage() {
   useEffect(() => {
     fetchTasks()
       .then(data => {
-        console.log('تسک‌های داشبورد:', data);
+        console.log('Dashboard Tasks:', data);
         setTasks(data);
       })
-      .catch(() => console.error('خطا در دریافت تسک‌ها برای داشبورد'));
+      .catch(() => console.error('Error Tasks'));
   }, []);
 
   const total = tasks.length;
@@ -22,14 +22,14 @@ export default function DashboardPage() {
   return (
     <div className="max-w-2xl mx-auto px-4 py-10 text-white">
       <h1 className="text-3xl font-bold mb-6 text-center text-gray-900 dark:text-white">
-  داشبورد تسک‌ها
+  Dashboard Tasks
 </h1>
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-8">
-        <StatCard label="کل تسک‌ها" value={total} color="bg-gray-700" />
-        <StatCard label="در انتظار" value={pending} color="bg-yellow-600" />
-        <StatCard label="در حال انجام" value={inProgress} color="bg-blue-600" />
-        <StatCard label="انجام شده" value={completed} color="bg-green-600" />
+        <StatCard label="All Tasks" value={total} color="bg-gray-700" />
+        <StatCard label="Pending" value={pending} color="bg-yellow-600" />
+        <StatCard label="In Progress" value={inProgress} color="bg-blue-600" />
+        <StatCard label="Completed" value={completed} color="bg-green-600" />
       </div>
 
      
